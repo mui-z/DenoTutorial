53cf04cfa1cd2f857239b208ac9ec832f45587f8
mui-z <93278577+mui-z@users.noreply.github.com>
2022-12-20 00:36:31 +0900

    add: add r/w files and unix cat progoram

---
+++ ReadAndWriteFiles/read.ts
@@ -0,0 +1,3 @@
+const text = await Deno.readTextFile("./peaple.json")
+console.log(text);
+
---
+++ ReadAndWriteFiles/write.ts
@@ -0,0 +1,12 @@
+function writeJson(path: string, data: object): string {
+  try {
+    Deno.writeTextFileSync(path, JSON.stringify(data));
+
+    return "Written to " + path;
+  } catch (e) {
+    return e.message;
+  }
+}
+
+console.log(writeJson("./data.json", { hello: "World" }));
+
